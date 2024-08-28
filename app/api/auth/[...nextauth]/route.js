30a0710efbcd783c8532f62077a518a05bab9d1f
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-08-29 01:08:06 +0700

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,45 +4,42 @@ import User from "../../../models/user";
 import bcrypt from 'bcryptjs';
 import { connectMongoDB } from "@/lib/mongodb";
 
-export const authOptions = {
+const options = {
     providers: [
-      CredentialsProvider({
-        name: "credentials",
-        credentials: {},
-  
-        async authorize(credentials) {
-          const { email, password } = credentials;
-  
-          try {
-            await connectMongoDB();
-            const user = await User.findOne({ email });
-  
-            if (!user) {
-              return null;
-            }
-  
-            const passwordsMatch = await bcrypt.compare(password, user.password);
-  
-            if (!passwordsMatch) {
-              return null;
-            }
-  
-            return user;
-          } catch (error) {
-            console.log("Error: ", error);
-          }
-        },
-      }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {},
+            async authorize(credentials) {
+                const { email, password } = credentials;
+
+                try {
+                    await connectMongoDB();
+                    const user = await User.findOne({ email });
+
+                    if (!user) {
+                        return null;
+                    }
+
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (!passwordsMatch) {
+                        return null;
+                    }
+
+                    return user;
+                } catch (error) {
+                    console.log("Error: ", error);
+                }
+            },
+        }),
     ],
     session: {
-      strategy: "jwt",
+        strategy: "jwt",
     },
     secret: process.env.NEXTAUTH_SECRET,
     pages: {
-      signIn: "/",
+        signIn: "/",
     },
-  };
+};
 
-const handler = NextAuth(authOptions);
-
-export{ handler as GET, handler as POST}
+export default NextAuth(options);
